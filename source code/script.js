b2ecc6e9c6506b312256d08a59a5e05a99355280
Ralph Andy Menz <andyralphmenz@gmail.com>
2024-07-03 19:05:31 +0000

    mistake

--- source code/script.js
+++ source code/script.js
@@ -236,13 +236,7 @@ let main = {
   },
 
   methods: {
-    gamesetup: function() {
-      $('.gamecell').attr('chess', 'null');
-      for (let gamepiece in main.variables.pieces) {
-        $('#' + main.variables.pieces[gamepiece].position).html(main.variables.pieces[gamepiece].img);
-        $('#' + main.variables.pieces[gamepiece].position).attr('chess', gamepiece);
-      }
-    },
+   
 
     moveoptions: function(selectedpiece) {
 
